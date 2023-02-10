e4b867b261e115a2bb64e713c247082ed3adc354
jooyeokkim <jooyeok.kim@datamaker.io>
2023-02-10 05:04:44 +0000

    20230210 image field changed to not null

--- snack/models.py
+++ snack/models.py
@@ -4,7 +4,7 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 
 class Snack(models.Model):
     name = models.CharField('NAME', max_length=50)
-    image = models.ImageField('IMAGE', upload_to='snack/images/', blank=True, null=True)
+    image = models.ImageField('IMAGE', upload_to='snack/images/')
     url = models.URLField('URL', max_length=500)
     description = models.CharField('DESCRIPTION', max_length=300, blank=True, help_text=' (개수 등 설명 추가)')
     is_accepted = models.BooleanField('IS_ACCEPTED', default=False)
